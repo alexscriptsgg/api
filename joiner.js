804f73ab7954ff2b7906ca11a66dcdf12995dbec
alexscriptsgg <abnk24715@gmail.com>
2025-10-25 11:44:36 +0300

    Update joiner.js

--- joiner.js
+++ joiner.js
@@ -1,1 +1,13 @@
+export default async function handler(req, res) {
+  const { placeId, gameInstanceId } = req.query;
+
+  if (!placeId || !gameInstanceId) {
+    return res.status(400).send("Missing placeId or gameInstanceId");
+  }
+
+  // Redirects to the Roblox app to join that specific server
+  const robloxLink = `roblox://placeId=${placeId}&gameInstanceId=${gameInstanceId}`;
+  res.writeHead(302, { Location: robloxLink });
+  res.end();
+}
 
